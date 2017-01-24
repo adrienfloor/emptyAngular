424c55b2d68dbc5277263c81c9cea5dcbe980c2a
adrienfloor <adriaandrum@gmail.com>
2017-01-24 11:08:13 +0100

    Quête terminée

--- js/controllers/mainController.js
+++ js/controllers/mainController.js
@@ -1,4 +1,18 @@
 // MAIN CONTROLLER
 function mainController($scope) {
-    
+
+    $scope.create = (info) => {
+        $scope.infos.push(info);
+        $scope.info = {};
+
+    };
+
+    $scope.infos = [{
+        nom: "",
+        prenom: "",
+        email: ""
+    }];
+
+
+
 }
