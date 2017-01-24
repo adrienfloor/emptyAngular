f65365e673a0dffea6b7940804e3b734723edfc4
adrienfloor <adriaandrum@gmail.com>
2017-01-24 11:15:31 +0100

    quête vraiment terminée

--- js/controllers/mainController.js
+++ js/controllers/mainController.js
@@ -1,18 +1,16 @@
 // MAIN CONTROLLER
 function mainController($scope) {
 
+  $scope.infos = [{
+      nom: "",
+      prenom: "",
+      email: ""
+  }];
+  $scope.info = {};
+
     $scope.create = (info) => {
         $scope.infos.push(info);
         $scope.info = {};
 
     };
-
-    $scope.infos = [{
-        nom: "",
-        prenom: "",
-        email: ""
-    }];
-
-
-
 }
